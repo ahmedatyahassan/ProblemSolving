2bdb23909bde83b81e407b2b05baea8b560c28ae
AhmedAtya74 <64374947+AhmedAtya74@users.noreply.github.com>
2021-04-04 16:35:15 +0200

    Update Search in Rotated Sorted Array II.cpp

--- LeetCode - Medium - Binary Search/Search in Rotated Sorted Array II.cpp
+++ LeetCode - Medium - Binary Search/Search in Rotated Sorted Array II.cpp
@@ -1,14 +1,28 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-      
-    
-        int first = lower_bound(nums.begin() , nums.end() , target) - nums.begin();
-        int last = upper_bound(nums.begin() + first , nums.end() , target) - nums.begin();
-     
-        if(last - 1 < first)
-            return {-1 , -1};
-        else
-            return {first , last - 1};
+    bool search(vector<int>& nums, int target) {
+         int left = 0, right =  nums.size()-1, mid;
+        
+        while(left<=right)
+        {
+            mid = (left + right) >> 1;
+            if(nums[mid] == target) return true;
+
+            // the only difference from the first one, trickly case, just updat left and right
+            if( (nums[left] == nums[mid]) && (nums[right] == nums[mid]) ) {++left; --right;}
+
+            else if(nums[left] <= nums[mid])
+            {
+                if( (nums[left]<=target) && (nums[mid] > target) ) right = mid-1;
+                else left = mid + 1; 
+            }
+            else
+            {
+                if((nums[mid] < target) &&  (nums[right] >= target) ) left = mid+1;
+                else right = mid-1;
+            }
+        }
+        return false;
     }
+    
 };
