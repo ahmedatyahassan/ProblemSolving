560daa91b08c483108afa40a5d2ea287bf439788
AhmedAtya74 <64374947+AhmedAtya74@users.noreply.github.com>
2021-07-10 08:11:54 +0200

    Create Find the Duplicate Number.cpp

---
+++ LeetCode/Binary Search/Find the Duplicate Number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        vector<int>vis(nums.size());
+        int i = 0;
+        for(; i < nums.size() ; i++)
+        {
+            if(vis[nums[i]] == 0)
+                vis[nums[i]] = 1;
+            else
+                break;
+        }
+        return nums[i];
+    }
+};
