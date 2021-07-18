8e9f80be47056b953affbe014fb68685c4e72949
AhmedAtya74 <64374947+AhmedAtya74@users.noreply.github.com>
2021-07-18 11:10:09 +0200

    Create Game with string.cpp

---
+++ Codeforces/Data Structures/Game with string.cpp
@@ -0,0 +1,58 @@
+
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <stack>
+
+using namespace std;
+#define ll           long long
+#define all(v)       (v).begin() , (v).end()
+#define v           vector <int>
+#define vi           vector <ll>
+#define vii          vector <pair<ll,ll>>
+#define ii           pair<ll,ll>
+#define sz(v)	     ((int)((v).size()))
+#define lp(i,a,b)    for(int i=a;i<b;i++)
+#define pb           push_back
+#define pf           push_front
+#define F            first
+#define S            second
+#define EPS			 1e-9
+#define endl         "\n"
+
+void init() {
+	cin.tie(0);
+	cin.sync_with_stdio(0);
+}
+
+void solve()
+{
+	string s; cin >> s;
+	stack<char>st;
+	st.push(s[0]);
+	int del = 0;
+	for (int i = 1; i < s.size(); i++)
+	{
+		if (!st.empty() && st.top() == s[i])
+			st.pop(), del++;
+		else
+			st.push(s[i]);
+	}
+	if (del & 1)
+		cout << "Yes \n";
+	else
+		cout << "No \n";
+}
+
+int main() {
+	init();
+
+	// int tt; cin >> tt;
+	// while (tt--)
+		solve();
+
+}
